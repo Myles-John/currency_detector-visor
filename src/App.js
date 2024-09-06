95f372dfc0d41bf133a7d08971d5f8ba3f7a5c6f
Myles-John <johnmyles523@gmail.com>
2024-09-06 23:19:51 +0000

    visual effect update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import VideoFeed from './VideoFeed';
 import AudioFeed from './AudioFeed';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>VISOR Object Detection</h1>
+        <h1>VISOR</h1>
       </header>
       <main>
         <VideoFeed />
